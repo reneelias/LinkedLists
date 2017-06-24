6cb9bd98e4ec417c36525040afafc61262cdc3bf
GMRDL8 <gmrstudent@outlook.com>
2017-06-24 15:51:10 -0700

    Added node to project

--- ReneLinkedLists/SinglyLinkedNode.cs
+++ ReneLinkedLists/SinglyLinkedNode.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ReneLinkedLists
 {
-    class SinglyLinkedNode
+    public class SinglyLinkedNode<T>
     {
+        public T Item { get; set; }
+        public SinglyLinkedNode<T> Next { get; set; }
+        public SinglyLinkedNode(T item, SinglyLinkedNode<T> next = null)
+        {
+            Item = item;
+            Next = next;
+        }
+
+        public void AddAfter(T itemToAdd)
+        {
+            SinglyLinkedNode<T> NextNode = new SinglyLinkedNode<T>(itemToAdd);
+            NextNode.Next = Next;
+            Next = NextNode;
+        }
+
+        public void RemoveNext()
+        {
+            Next = Next.Next;
+        }
     }
 }
